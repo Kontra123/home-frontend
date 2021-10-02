133ef991cfa4e33f7366c164a7648c21d32bdcc0
amir-brightsource <amir@brightsource.com>
2021-10-02 19:17:41 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './app.scss';
 import { Route, Switch, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import MainView from './views/mainView/MainView.view';
-/
+
 const App = () => {
     
     return (
