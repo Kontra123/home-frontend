d43d79fe48d617518cf8e6ee86458cb64c6ce319
amir-brightsource <amir@brightsource.com>
2021-10-02 17:59:26 +0300

    Updates

---
+++ precache-manifest.1b383a2e155aa280d4fe57b90c7469da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02197c5411bd1c22dfce55e5a41472a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0a4b2cd751e0d5e97d9",
+    "url": "/static/css/main.6216523b.chunk.css"
+  },
+  {
+    "revision": "cdbfbd254be8b5992f57",
+    "url": "/static/js/2.b93da58b.chunk.js"
+  },
+  {
+    "revision": "b0a4b2cd751e0d5e97d9",
+    "url": "/static/js/main.0b789877.chunk.js"
+  },
+  {
+    "revision": "cfcb1fa1b8f6c74d19ac",
+    "url": "/static/js/runtime~main.95da5e5a.js"
+  },
+  {
+    "revision": "1b4ccf59f028b3036260edf8dec8587f",
+    "url": "/static/media/active.1b4ccf59.svg"
+  },
+  {
+    "revision": "c0b72c3fdf3c2989c6bef8187d408985",
+    "url": "/static/media/arrow-right.c0b72c3f.svg"
+  },
+  {
+    "revision": "00ed6d431b324d7f4236b0ab678b88bb",
+    "url": "/static/media/help.00ed6d43.svg"
+  }
+]);
